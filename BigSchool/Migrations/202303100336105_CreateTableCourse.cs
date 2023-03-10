814481baa9e592ad7f7619a48669568924d6038a
ADMIN <ADMIN@NGUYEN-DINH>
2023-03-10 10:54:36 +0700

    Add project files.

---
+++ BigSchool/Migrations/202303100336105_CreateTableCourse.cs
@@ -0,0 +1,34 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableCourse : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Courses", "CategoryID_Id", "dbo.Categories");
+            DropForeignKey("dbo.Courses", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "Category_Id" });
+            DropIndex("dbo.Courses", new[] { "CategoryID_Id" });
+            RenameColumn(table: "dbo.Courses", name: "Category_Id", newName: "CategoryID");
+            AlterColumn("dbo.Courses", "CategoryID", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Courses", "CategoryID");
+            AddForeignKey("dbo.Courses", "CategoryID", "dbo.Categories", "Id", cascadeDelete: true);
+            DropColumn("dbo.Courses", "CategoryID_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Courses", "CategoryID_Id", c => c.Byte(nullable: false));
+            DropForeignKey("dbo.Courses", "CategoryID", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "CategoryID" });
+            AlterColumn("dbo.Courses", "CategoryID", c => c.Byte());
+            RenameColumn(table: "dbo.Courses", name: "CategoryID", newName: "Category_Id");
+            CreateIndex("dbo.Courses", "CategoryID_Id");
+            CreateIndex("dbo.Courses", "Category_Id");
+            AddForeignKey("dbo.Courses", "Category_Id", "dbo.Categories", "Id");
+            AddForeignKey("dbo.Courses", "CategoryID_Id", "dbo.Categories", "Id", cascadeDelete: true);
+        }
+    }
+}
---
+++ BigSchool/Migrations/202303100336569_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (4,'Development')");
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (5,'Business')");
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (6,'Marketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
