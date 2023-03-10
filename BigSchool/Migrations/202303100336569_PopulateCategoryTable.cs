67c40c341b498ced4ea42f30fe633ef6e36e39dc
Lê Nguyễn Đình Nguyên <88964296+dinhnguyen1812002@users.noreply.github.com>
2023-03-10 21:39:57 +0700

    Update 202303100336569_PopulateCategoryTable.cs

--- BigSchool/Migrations/202303100336569_PopulateCategoryTable.cs
+++ BigSchool/Migrations/202303100336569_PopulateCategoryTable.cs
@@ -7,9 +7,9 @@
     {
         public override void Up()
         {
-            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (4,'Development')");
-            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (5,'Business')");
-            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (6,'Marketing')");
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (1,'Development')");
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (2,'Business')");
+            Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (3,'Marketing')");
         }
         
         public override void Down()
