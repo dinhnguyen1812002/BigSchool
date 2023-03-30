8f95467c447e0271e78f669db43340445fd9f714
Dinhnguyen1812002@gmail.com <Dinhnguyen1812002@gmail.com>
2023-03-30 22:56:55 +0700

    Tuần 3

--- BigSchool/Models/ApplicationDbContext.cs
+++ BigSchool/Models/ApplicationDbContext.cs
@@ -36,6 +36,7 @@ namespace BigSchool.Models
                .HasMany(u => u.followees)
                .WithRequired(f => f.Follower)
                .WillCascadeOnDelete(false);
+
             base.OnModelCreating(modelBuilder);
         }
 
